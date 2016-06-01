912284c21d8eb265848ae1cdad883453a2e8df67
riko <rikomailbox@gmail.com>
2016-06-01 11:24:32 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by riko on 01.06.2016.
+ */
+public class Main {
+    public static void main(String[] args){
+
+    }
+
+    public static void regex_match(String pathToTxtFileWithDataList, String regex){
+        
+    }
+}
