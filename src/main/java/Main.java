1a549015764738fec7f0d8bd255dbd6a96cee6f4
riko <rikomailbox@gmail.com>
2016-06-01 17:51:35 +0300

    final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        regex_match("in.txt", "(?i)^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|0x[0-9a-f]{2}|0[0-7]{3})\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|0x[0-9a-f]{2}|0[0-7]{3})\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|0x[0-9a-f]{2}|0[0-7]{3})\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|0x[0-9a-f]{2}|0[0-7]{3})|0x[0-9a-f]{8}|(1|[1-7][0-7]|[1-3][0-7]{2})[0-7]{8}|[1-9][0-9]{7,9})$");
+        String regexIP4 = "(?i)^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|0x[0-9a-f]{2}|0[0-7]{3})\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|0x[0-9a-f]{2}|0[0-7]{3})\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|0x[0-9a-f]{2}|0[0-7]{3})\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|0x[0-9a-f]{2}|0[0-7]{3})|0x[0-9a-f]{8}|(1|[1-7][0-7]|[1-3][0-7]{2})[0-7]{8}|[1-9][0-9]{7,9})$";
+        String regexDomain = "(?i)^https?\\:\\/\\/([a-z0-9]+|[a-z0-9][a-z0-9\\-?\\.]*[a-z0-9])\\.[a-z]+\\/?$";
+
+        regex_match("ip.txt", regexIP4);
+        regex_match("domain.txt", regexDomain);
     }
 
     public static void regex_match(String pathToTxtFileWithDataList, String regex) throws IOException {
@@ -12,10 +16,8 @@ public class Main {
             newFileName = pathToTxtFileWithDataList.substring(0, pathToTxtFileWithDataList.lastIndexOf('.')) + "_result" + pathToTxtFileWithDataList.substring(pathToTxtFileWithDataList.lastIndexOf('.'));
         else
             newFileName = pathToTxtFileWithDataList + "_result";
-        File resultFile = new File(newFileName);
-        //resultFile.createNewFile();
 
-        BufferedWriter writer = new BufferedWriter(new FileWriter(resultFile));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(newFileName));
         Scanner scanner = new Scanner(new File(pathToTxtFileWithDataList));
 
         while (scanner.hasNext()) {
@@ -36,9 +38,4 @@ public class Main {
         writer.close();
     }
 
-//    public static boolean check(String testedString, String regex){
-//        Pattern p = Pattern.compile(regex);
-//        Matcher m = p.matcher(testedString);
-//        return m.matches();
-//    }
 }
